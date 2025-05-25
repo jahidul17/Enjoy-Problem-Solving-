7f417ab18a9e47452bcb396399e28a5ec2e8a331
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2025-05-25 23:58:57 +0600

    Easy programming

---
+++ Hacker Rank/Text_Wrap.py
@@ -0,0 +1,14 @@
+import textwrap
+
+def wrap(text,width):
+    return textwrap.fill(text, width)
+
+
+
+if __name__ == '__main__':
+    text,width=input(),int(input())
+
+    result=wrap(text,width)
+
+
+
