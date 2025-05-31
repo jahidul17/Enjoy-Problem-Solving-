b8386ef62400c2c12bcd8949b05fcdb06756346f
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2025-05-31 23:56:34 +0600

    Easy programming

--- Hacker Rank/itertools_product.py
+++ Hacker Rank/itertools_product.py
@@ -1,2 +1,23 @@
 
+from itertools import product
+
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+# axb=product(a,b)
+
+# for i in axb:
+#     print(i, end=" ")
+
+
+# or 
+
+pack=list(product(a,b))
+# print(pack)
+
+# The asterisk * is the unpacking operator.
+#unpack
+print(*pack)
+
+
 
