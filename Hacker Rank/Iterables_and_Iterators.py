b41f28a02480abccdbd0744c2478030758ed6b16
jahidul17 <jahidulislam.khan1234@gmail.com>
2025-06-07 00:21:00 +0600

    Easy programming

--- Hacker Rank/Iterables_and_Iterators.py
+++ Hacker Rank/Iterables_and_Iterators.py
@@ -1,3 +1,15 @@
+from itertools import *
+n=input()
 
+s=list(input().split())
 
-print("Ok done")
+k=int(input())
+c=0
+
+for i in combinations(s,k):
+    if 'a' in i:
+        c=c+1
+print(list(combinations(s,k)))
+
+# x=c/len(list(combinations(s,k)))  
+# print(f'{x:.4f}')
