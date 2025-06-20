1ee2a0e65043c1d7f6283801c3418f753c6e173f
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2025-06-20 23:56:32 +0600

    Easy programming

--- Hacker Rank/Collections_namedtuple.py
+++ Hacker Rank/Collections_namedtuple.py
@@ -1,15 +1,30 @@
+# from collections import namedtuple
+
+# n=int(input())
+
+# fields=input().split()
+# total_marks=0
+# for i in range(n):
+#     # students=namedtuple('my_student','')
+#     students = namedtuple('my_student', fields)
+#     MARKS, CLASS, NAME, ID = input().split()
+#     my_student = students(MARKS, CLASS, NAME, ID)
+#     total_marks += int(my_student.MARKS)
+# print((f'{total_marks / n:.2f}'))
+
 from collections import namedtuple
 
-n=int(input())
+n = int(input())
+fields = input().split()
 
-fields=input().split()
-total_marks=0
-for i in range(n):
-    # students=namedtuple('my_student','')
-    students = namedtuple('my_student', fields)
-    MARKS, CLASS, NAME, ID = input().split()
-    my_student = students(MARKS, CLASS, NAME, ID)
-    total_marks += int(my_student.MARKS)
-print((total_marks / n))
-    
+Student = namedtuple('Student', fields)  # Define namedtuple once
+
+total_marks = 0
+for _ in range(n):
+    data = input().split()
+    student = Student(*data)
+    total_marks += int(student.MARKS)
+
+print(f'{total_marks / n:.2f}')
+
 
