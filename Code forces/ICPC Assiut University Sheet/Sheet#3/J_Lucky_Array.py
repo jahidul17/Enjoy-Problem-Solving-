b2e616d2548053db5753435b7acb8136c31e09a8
jahidul17 <jahidulislam.khan1234@gmail.com>
2025-10-09 23:58:17 +0600

    Easy programming

--- Code forces/ICPC Assiut University Sheet/Sheet#3/J_Lucky_Array.py
+++ Code forces/ICPC Assiut University Sheet/Sheet#3/J_Lucky_Array.py
@@ -3,6 +3,12 @@ n=int(input())
 num=list(map(int, input().split()))
 
 min=min(num)
+count=num.count(min)
 
-print(min)
+if(count%2==0):
+    # print(count)
+    print("Unlucky")
+elif(count%2!=0):
+    # print(count)
+    print("Lucky")
 
