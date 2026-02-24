750886b8f03342d07c1c03e126f032a04cba729e
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2026-02-24 23:49:16 +0600

    solved smallest pair problem

--- Code forces/ICPC Assiut University Sheet/Sheet#3/I_Smallest_Pair.py
+++ Code forces/ICPC Assiut University Sheet/Sheet#3/I_Smallest_Pair.py
@@ -1,3 +1,21 @@
+t=int(input())
+
+for _ in range(t):
+    n=int(input())
+    arr=list(map(int,input().split()))
+
+    answer=float('inf')
+    
+    for i in range(n):
+        for j in range(i+1,n):
+            value=arr[i]+arr[j]+(j-i)
+            answer=min(answer,value)
+            
+    print(answer)
+
+
+        
+
 
 
 
