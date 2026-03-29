5e1812ee900f67617d888c75769dd709c4d5ba7c
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2026-03-29 23:56:29 +0600

    Happy coder

--- Code forces/ICPC Assiut University Sheet/Sheet#4/R_String_Score.py
+++ Code forces/ICPC Assiut University Sheet/Sheet#4/R_String_Score.py
@@ -1,5 +1,41 @@
 n=int(input())
-s=input()
-print(s)
+# s=input()
+# print(s)
+# n = int(input())
+s = list(input())
 
+score = 0
+i = 0
+
+while i < len(s):
+    if s[i] == 'V':
+        score += 5
+
+    elif s[i] == 'W':
+        score += 2
+
+    elif s[i] == 'X':
+        # next character remove
+        if i + 1 < len(s):
+            s.pop(i + 1)
+
+    elif s[i] == 'Y':
+        # move next character to end
+        if i + 1 < len(s):
+            ch = s.pop(i + 1)
+            s.append(ch)
+
+    elif s[i] == 'Z':
+        # check next character
+        if i + 1 < len(s):
+            if s[i + 1] == 'V':
+                score //= 5
+                s.pop(i + 1)
+            elif s[i + 1] == 'W':
+                score //= 2
+                s.pop(i + 1)
+
+    i += 1
+
+print(score)
 
