dc05f98f9ca60cf6c5caaf627fa61f295108ac8d
jahidul17 <41232668+jahidul17@users.noreply.github.com>
2026-04-05 23:49:05 +0600

    Happy coder

--- Code forces/ICPC Assiut University Sheet/Sheet#4/R_String_Score.py
+++ Code forces/ICPC Assiut University Sheet/Sheet#4/R_String_Score.py
@@ -1,3 +1,5 @@
+#TLE issue
+
 n=int(input())
 # s=input()
 # print(s)
@@ -15,40 +17,26 @@ while i<len(s):
         score +=2
         
     elif s[i]== 'X':
-        score +=
+        if i+1<len(s):
+            s.pop(i+1)
+            
+    elif s[i]=='Y':
+        if i+1<len(s):
+            ch=s.pop(i+1)
+            s.append(ch)
+    
+    elif s[i]=='Z':
+        if i+1<len(s):
+            if s[i+1]=='V':
+                score//=5
+                s.pop(i+1)
+            if s[i+1]=='W':
+                score//=2
+                s.pop(i+1)
+                
+    i+=1
 
-# score = 0
-# i = 0
+print(score)
 
-# while i < len(s):
-#     if s[i] == 'V':
-#         score += 5
 
-#     elif s[i] == 'W':
-#         score += 2
-
-#     elif s[i] == 'X':
-#         # next character remove
-#         if i + 1 < len(s):
-#             s.pop(i + 1)
-
-#     elif s[i] == 'Y':
-#         # move next character to end
-#         if i + 1 < len(s):
-#             ch = s.pop(i + 1)
-#             s.append(ch)
-
-#     elif s[i] == 'Z':
-#         # check next character
-#         if i + 1 < len(s):
-#             if s[i + 1] == 'V':
-#                 score //= 5
-#                 s.pop(i + 1)
-#             elif s[i + 1] == 'W':
-#                 score //= 2
-#                 s.pop(i + 1)
-
-#     i += 1
-
-# print(score)
 
